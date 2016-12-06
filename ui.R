3ac29854764833b547a3aaa572159bd78cfa1c30
Joo Y. Park <jyp0328@D-10-155-103-253.dhcp4.washington.edu>
2016-12-05 16:15:00 -0800

    reverted change in organized data

--- ui.R
+++ ui.R
@@ -35,21 +35,20 @@ shinyUI(fluidPage(
     # ***Add your tab panels below these two comments***
     # (Note: A comma is required after each one, except for the last. You will need to put in a comma above when adding the next tabPanel)
     tabPanel("Attribute Comparison",
-             titlePanel("Attribute Relationships"),
-             sidebarLayout(
-               sidebarPanel(
-                 selectInput("X_Value", label = h2("X Variable"),
+              titlePanel("Attribute Relationships"),
+              sidebarLayout(
+                sidebarPanel(
+                  selectInput("X_Value", label = h2("X Variable"),
                              choices = player_attributes,
                              selected = player_attributes[1]),
-                 selectInput("Y_Value", label = h2("Y Variable"),
+                  selectInput("Y_Value", label = h2("Y Variable"),
                              choices = player_attributes,
                              selected = player_attributes[2])
+                 ),
+                 mainPanel(
+                   plotOutput('scatter')
+                 )
                )
-             ),
-               mainPanel(
-                 plotOutput('scatter')
-               )
-             )
              )
     )
-  )
+    ))
