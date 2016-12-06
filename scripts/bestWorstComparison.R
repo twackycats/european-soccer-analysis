1cac691f673a3ba441f1bc4b7c294a2c9d5f2006
Nick Hjelle <nhjell01@seattlecentral.edu>
2016-12-06 14:48:39 -0800

    modified bestWorstComparison.R

--- scripts/bestWorstComparison.R
+++ scripts/bestWorstComparison.R
@@ -2,20 +2,21 @@ library(dplyr)
 library(shiny)
 
 record <- function(use, input){
+  print(input)
   #Uses a csv file that reflects current data
-  use <- use %>% arrange(desc(date))
-  use <- na.omit(use)
-  use <- subset(use, !duplicated(use[,4]))
+  use <- use %>% group_by(player_fifa_api_id) %>% top_n(n=1, wt = date)
   
   #best player with complete relevent information at hand
-  best <- use %>% filter(input==max(input))
+  use <- use %>% select_("player_name", input)
+  use <- subset(use, select = -player_fifa_api_id)
+  colnames(use) <- c('name', 'var')
+  best <- use %>% top_n(n=1, wt=var)
+
   #worst player with complete relevent information at hand
-  worst <- use %>% filter(input==min(input))
+  worst <- use %>% arrange(var) %>% head(1)
+
   
-  merged <- data.frame(best)
-  merged[2,] <-worst
-  
-  ret <- paste0("The best ", input, "player was: ", merged[1,4], " while the worst was: ", merged[2,4])
+  ret <- paste0("The best ", input, " player was: ", best$name, " while the worst was: ", worst$name)
   
   return(ret)
 }
